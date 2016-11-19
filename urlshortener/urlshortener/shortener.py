209a3669112348ce6c597b12dfa2aac362a8371c
Максим Музафаров <m.muzafarov@gmail.com>
2016-11-19 19:06:51 +0300

    Init: redis role, simple shortener service

---
+++ urlshortener/urlshortener/shortener.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from flask import Flask, render_template, request, abort, redirect, url_for
+from redis.sentinel import Sentinel
+app = Flask(__name__)
+app.sentinel = Sentinel([('localhost', 26379)], socket_timeout=0.1)
+app.store = app.sentinel.master_for('mymaster', socket_timeout=0.1)
+
+ID = 0
+
+def encode(num):
+    alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if num == 0:
+        return alphabet[0]
+    arr = []
+    base = len(alphabet)
+    while num:
+        num, rem = divmod(num, base)
+        arr.append(alphabet[rem])
+    arr.reverse()
+    return ''.join(arr)
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    global ID
+    if request.method == "POST":
+        if 'url' not in request.form:
+            abort(400)
+        code = encode(ID)
+        ID += 1
+        app.store.set(code, request.form['url'])
+        return render_template("result.html", url=request.url + code)
+    return render_template("index.html")
+
+@app.route("/<url>")
+def redir(url):
+    redirect_url = app.store.get(url)
+    if redirect_url:
+        return redirect(redirect_url)
+    else:
+        abort(404)
+
+if __name__ == '__main__':
+    app.run(debug=True)
