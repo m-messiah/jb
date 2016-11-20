801e9ef60404e80fc7ee4df2573d05190881698c
Максим Музафаров <m.muzafarov@gmail.com>
2016-11-20 10:28:45 +0300

    Deploy shortener service

--- urlshortener/shortener/shortener.py
+++ urlshortener/shortener/shortener.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
-
 from flask import Flask, render_template, request, abort, redirect, url_for
 from redis.sentinel import Sentinel
+from uuid import uuid4
 app = Flask(__name__)
-app.sentinel = Sentinel([('192.168.100.9', 26379)], socket_timeout=0.1)
+app.sentinel = Sentinel([('127.0.0.1', 26379)], socket_timeout=0.1)
 app.store = app.sentinel.master_for('mymaster', socket_timeout=0.1)
 
-ID = 0
 
-def encode(num):
-    alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+def get_code():
+    num = int(uuid4())
+    alphabet = ("0123456789abcdefghijklmnopqrstuvwxyz"
+                " ABCDEFGHIJKLMNOPQRSTUVWXYZ-_")
     if num == 0:
         return alphabet[0]
     arr = []
@@ -18,7 +19,11 @@ def encode(num):
         num, rem = divmod(num, base)
         arr.append(alphabet[rem])
     arr.reverse()
-    return ''.join(arr)
+    code = ''.join(arr)
+    if app.store.get(code):
+        return get_code()
+    return code
+
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -26,11 +31,13 @@ def index():
     if request.method == "POST":
         if 'url' not in request.form:
             abort(400)
-        code = encode(ID)
-        ID += 1
+
+        code = get_code()
+
         app.store.set(code, request.form['url'])
         return render_template("result.html", url=request.url + code)
     return render_template("index.html")
+
 
 @app.route("/<url>")
 def redir(url):
@@ -39,6 +46,7 @@ def redir(url):
         return redirect(redirect_url)
     else:
         abort(404)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
